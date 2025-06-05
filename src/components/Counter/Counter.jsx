a6dbe568f506c247c109feebd19ac0856228c4f2
xavi <antunez.xavi.grao@gmail.com>
2025-06-05 13:13:33 +0200

    AUTOCOMMIT

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,6 +1,13 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../../features/counter/counterSlice";
+
 const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   const incrementCounter = () => {
     console.log("Counter incremented");
+    dispatch(increment())
   };
   const decrementCounter = () => {
     console.log("Counter decremented");
@@ -8,7 +15,7 @@ const Counter = () => {
   return (
     <div>
       <button onClick={incrementCounter}>+</button>
-      <h1>0</h1>
+      <h1>{count}</h1>
       <button onClick={decrementCounter}>-</button>
     </div>
   );
