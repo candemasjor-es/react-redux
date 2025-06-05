c0998830365ef03304c64cc276c89d279bf6be19
xavi <antunez.xavi.grao@gmail.com>
2025-06-05 13:09:49 +0200

    AUTOCOMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Counter } from "./components/Counter/Counter";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "./App.css";
 
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
       <Counter />
-    </>
+    </Provider>
   );
 }
 
